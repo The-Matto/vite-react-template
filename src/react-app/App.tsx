445f5bf51bae564740df8417ce32741825ee444d
Matthew Heritage <destroyer273@hotmail.co.uk>
2025-08-22 17:13:44 +0100

    Added test message

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -12,7 +12,7 @@ function App() {
   const [name, setName] = useState("unknown");
 
   return (
-    <>
+    <>Hello
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
