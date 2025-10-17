4f749fef26e53c4c5c6d5be3b3f8c640a0dd2b37
Matthew Heritage <destroyer273@hotmail.co.uk>
2025-10-17 18:12:15 +0100

    Added NavBar, project cleanup

---
+++ src/react-app/components/layout-component.tsx
@@ -0,0 +1,25 @@
+﻿import NavBar from "./navbar-component.jsx.tsx";
+import {ReactNode} from "react";
+import {Route, Routes} from "react-router";
+
+
+export default function Layout({children}: { children: ReactNode }) {
+    return (
+        <>
+            <NavBar/>
+            <Routes>
+                <Route path="/" element={<p>Home</p>}/>
+
+
+                <Route path="/projects" element={<p>Projects</p>}/>
+                <Route path="/about" element={<p>About</p>}/>
+                <Route path="/contact" element={<p>Contact Me</p>}/>
+
+
+                <Route path="*" element={<p>Not Found</p>}/>
+            </Routes>
+            {children}
+
+        </>
+    )
+}
---
+++ src/react-app/components/nav-clickable-link.tsx
@@ -0,0 +1,14 @@
+﻿export default function NavLink({path, text, renderAsBlock}: { path: string, text: string, renderAsBlock?: boolean }) {
+
+    const classes = `rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5 hover:text-orange-500`;
+    return (
+        <a
+
+            bg-gray-900
+            href={path}
+            className={`${classes} ${renderAsBlock && 'block'}`}
+        >
+            {text}
+        </a>
+    )
+}
